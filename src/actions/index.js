6f827adb20b21781c50dbdc4ad59e6bdd704f2e2
Ishika Singhal <ishikasinghal2642005@gmail.com>
2025-06-10 23:24:29 +0530

    selection and rejection feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,46 +13,81 @@ export async function createProfile(formData, pathToRevalidate) {
   revalidatePath(pathToRevalidate);
 }
 
-
-export async function fetchProfile(id){
-    await connectToDB();
-    const result = await Profile.findOne({userId : id})
-    return JSON.parse(JSON.stringify(result))
+export async function fetchProfile(id) {
+  await connectToDB();
+  const result = await Profile.findOne({ userId: id });
+  return JSON.parse(JSON.stringify(result));
 }
 
-export async function postNewJob(formData,pathToRevalidate){
+export async function postNewJob(formData, pathToRevalidate) {
   await connectToDB();
   await Job.create(formData);
-  revalidatePath(pathToRevalidate)
+  revalidatePath(pathToRevalidate);
 }
 
-export async function  fetchJobForRecruiter(id){
+export async function fetchJobForRecruiter(id) {
   await connectToDB();
-  const result = await Job.find({recruiterId : id})
-  return JSON.parse(JSON.stringify(result))
+  const result = await Job.find({ recruiterId: id });
+  return JSON.parse(JSON.stringify(result));
 }
 
-export async function  fetchJobForCandidate(id){
+export async function fetchJobForCandidate(id) {
   await connectToDB();
-  const result = await Job.find({})
-  return JSON.parse(JSON.stringify(result))
+  const result = await Job.find({});
+  return JSON.parse(JSON.stringify(result));
 }
 
-export async function createJobApplication(data , pathToRevalidate){
+export async function createJobApplication(data, pathToRevalidate) {
   await connectToDB();
   await Application.create(data);
-  revalidatePath(pathToRevalidate)
+  revalidatePath(pathToRevalidate);
 }
 
-export async function fetchApplicationForCandidate(id){
+export async function fetchApplicationForCandidate(id) {
   await connectToDB();
-  const result = await Application.find({candidateUserId : id})
-  return JSON.parse(JSON.stringify(result))
+  const result = await Application.find({ candidateUserId: id });
+  return JSON.parse(JSON.stringify(result));
 }
 
-export async function fetchApplicationForRecruiter(id){
+export async function fetchApplicationForRecruiter(id) {
   await connectToDB();
-  const result = await Application.find({recruiterUserID : id})
-  return JSON.parse(JSON.stringify(result))
+  const result = await Application.find({ recruiterUserID: id });
+  return JSON.parse(JSON.stringify(result));
 }
 
+export async function getCandidateDetailsById(id) {
+  await connectToDB();
+  const result = await Profile.findOne({ userId: id });
+  return JSON.parse(JSON.stringify(result));
+}
+
+export async function updateJobApplication(data, pathToRevalidate) {
+  await connectToDB();
+  const {
+    recruiterUserID,
+    name,
+    email,
+    candidateUserId,
+    status,
+    jobID,
+    _id,
+    jobApplicationDate,
+  } = data;
+
+  const result = await Application.findOneAndUpdate(
+    { _id: _id },
+    {
+      recruiterUserID: recruiterUserID,
+      name: name,
+      email: email,
+      candidateUserId: candidateUserId,
+      status: status,
+      jobID: jobID,
+      jobApplicationDate: jobApplicationDate,
+    },
+    {
+      new: true,
+    }
+  );
+  revalidatePath(pathToRevalidate);
+}
--- src/components/CandidateList.jsx
+++ src/components/CandidateList.jsx
@@ -1,15 +1,60 @@
 "use client";
 
+import { getCandidateDetailsById, updateJobApplication } from "@/actions";
 import { Button } from "./ui/button";
 import { Dialog, DialogContent } from "./ui/dialog";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const CandidateList = ({
   currentCandidateDetails,
   setCurrentCandidateDetails,
   showCurrentCandidateDetailsModal,
   setShowCurrentCandidateDetailsModal,
-  jobApplication
+  jobApplication,
 }) => {
+  async function handleFetchCandidateDetails(id) {
+    const data = await getCandidateDetailsById(id);
+    if (data) {
+      setCurrentCandidateDetails(data);
+    }
+    setShowCurrentCandidateDetailsModal(true);
+  }
+
+  async function handleUpdateJobStatus(currentstatus) {
+    let cpyjobApplicants = [...jobApplication];
+    console.log(cpyjobApplicants);
+    const indexOfCurrentJobApplicant = cpyjobApplicants.findIndex(
+      (item) =>
+        item.
+candidateUserId === currentCandidateDetails?.userId
+    );
+
+    const jobApplicanToUpdate = {
+      ...cpyjobApplicants[indexOfCurrentJobApplicant],
+      status : cpyjobApplicants[indexOfCurrentJobApplicant].status.concat(currentstatus)
+    }
+    
+    await updateJobApplication(jobApplicanToUpdate , '/jobs')
+  }
+
+  async function handlePreviewResume() {
+    const { data } = await supabase.storage
+      .from("jobportal")
+      .getPublicUrl(currentCandidateDetails?.candidateInfo?.resume);
+    const a = document.createElement("a");
+    a.href = data?.publicUrl;
+
+    a.setAttribute("download", "Resume.pdf");
+    a.setAttribute("target", "_blank");
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
   return (
     <>
       <div className="grid grid-cols-1 gap-3 p-10 md:grid-cols-2 lg:grid-cols-3">
@@ -20,7 +65,14 @@ const CandidateList = ({
                   <h3 className="text-lg font-bold">
                     {jobApplicationItem?.name}
                   </h3>
-                  <Button className="disabled:opacity-65 flex h-11 items-center justify-center px-5" onClick={()=>setShowCurrentCandidateDetailsModal(true)}>
+                  <Button
+                    className="disabled:opacity-65 flex h-11 items-center justify-center px-5"
+                    onClick={() =>
+                      handleFetchCandidateDetails(
+                        jobApplicationItem?.candidateUserId
+                      )
+                    }
+                  >
                     View Profile
                   </Button>
                 </div>
@@ -29,11 +81,130 @@ const CandidateList = ({
           : null}
       </div>
 
-      <Dialog open={showCurrentCandidateDetailsModal} onOpenChange={setShowCurrentCandidateDetailsModal}>
+      <Dialog
+        open={showCurrentCandidateDetailsModal}
+        onOpenChange={() => {
+          setShowCurrentCandidateDetailsModal(false);
+          setCurrentCandidateDetails(null);
+        }}
+      >
         <DialogContent>
-           
+          <div>
+            <h1 className="text-2xl font-bold dark:text-white text-black">
+              {currentCandidateDetails?.candidateInfo?.name},{" "}
+              {currentCandidateDetails?.email}
+            </h1>
+            <p className="text-xl font-medium dark:text-white text-black">
+              {currentCandidateDetails?.candidateInfo?.currentCompany}
+            </p>
+            <p className="text-sm font-normal dark:text-white text-black">
+              {currentCandidateDetails?.candidateInfo?.currentJobLocation}
+            </p>
+            <p className="dark:text-white">
+              Total Experience:
+              {currentCandidateDetails?.candidateInfo?.totalExperience} Years
+            </p>
+            <p className="dark:text-white">
+              Salary: {currentCandidateDetails?.candidateInfo?.currentSalary}{" "}
+              LPA
+            </p>
+            <p className="dark:text-white">
+              Notice Period:{" "}
+              {currentCandidateDetails?.candidateInfo?.noticePeriod} Days
+            </p>
+            <div className="flex items-center gap-4 mt-6">
+              <h1 className="dark:text-white">Previous Companies</h1>
+              <div className="flex flex-wrap items-center gap-4 mt-6">
+                {currentCandidateDetails?.candidateInfo?.previousCompanies
+                  ?.split(",")
+                  .map((skillItem) => (
+                    <div className="w-[100px] dark:bg-white flex justify-center items-center h-[35px] bg-black rounded-[4px]">
+                      <h2 className="text-[13px]  dark:text-black font-medium text-white">
+                        {skillItem}
+                      </h2>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="flex flex-wrap gap-4 mt-6">
+              {currentCandidateDetails?.candidateInfo?.skills
+                ?.split(",")
+                .map((skillItem) => (
+                  <div className="w-[100px] dark:bg-white flex justify-center items-center h-[35px] bg-black rounded-[4px]">
+                    <h2 className="text-[13px] dark:text-black font-medium text-white">
+                      {skillItem}
+                    </h2>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="flex gap-3">
+            <Button
+              onClick={handlePreviewResume}
+              className=" flex h-11 items-center justify-center px-5"
+            >
+              Resume
+            </Button>
+            <Button
+              onClick={() => handleUpdateJobStatus("selected")}
+              className=" disabled:opacity-65 flex h-11 items-center justify-center px-5"
+              disabled={
+                jobApplication
+                  .find(
+                    (item) =>
+                      item.candidateUserID === currentCandidateDetails?.userId
+                  )
+                  ?.status.includes("selected") ||
+                jobApplication
+                  .find(
+                    (item) =>
+                      item.candidateUserID === currentCandidateDetails?.userId
+                  )
+                  ?.status.includes("rejected")
+                  ? true
+                  : false
+              }
+            >
+              {jobApplication
+                .find(
+                  (item) =>
+                    item.candidateUserId === currentCandidateDetails?.userId
+                )
+                ?.status.includes("selected")
+                ? "Selected"
+                : "Select"}
+            </Button>
+            <Button
+              onClick={() => handleUpdateJobStatus("rejected")}
+              className=" disabled:opacity-65 flex h-11 items-center justify-center px-5"
+              disabled={
+                jobApplication
+                  .find(
+                    (item) =>
+                      item.candidateUserId === currentCandidateDetails?.userId
+                  )
+                  ?.status.includes("selected") ||
+                jobApplication
+                  .find(
+                    (item) =>
+                      item.candidateUserId === currentCandidateDetails?.userId
+                  )
+                  ?.status.includes("rejected")
+                  ? true
+                  : false
+              }
+            >
+              {jobApplication
+                .find(
+                  (item) =>
+                    item.candidateUserID === currentCandidateDetails?.userId
+                )
+                ?.status.includes("rejected")
+                ? "Rejected"
+                : "Reject"}
+            </Button>
+          </div>
         </DialogContent>
-
       </Dialog>
     </>
   );
